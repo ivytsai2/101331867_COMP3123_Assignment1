a3db7df3e7daed0c6dc4a5376806e9d56d4018f7
DESKTOP-557UREB\Ivy <ivytsai2@gmail.com>
2022-12-04 01:55:41 -0500

    change error msg

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,13 +5,19 @@ const verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    return res.status(401).send({
+      status: false,
+      message: "Token not found..."
+    });
   }
   try {
     const decoded = jwt.verify(token, "my_secret_key");
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(403).send({
+      status: false,
+      message: "Invalid Token"
+    });
   }
   return next();
 };
